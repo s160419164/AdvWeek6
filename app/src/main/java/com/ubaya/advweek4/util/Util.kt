64985c28542a6e929b1944fe89c178c15ccc58fc
Bayu <s160419164.student.ubaya.ac.id>
2022-06-09 21:56:44 +0700

    week 10

--- app/src/main/java/com/ubaya/advweek4/util/Util.kt
+++ app/src/main/java/com/ubaya/advweek4/util/Util.kt
@@ -1,5 +1,9 @@
 package com.ubaya.advweek4.util
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
@@ -26,3 +30,15 @@ fun ImageView.loadImage(url: String?, progressBar:ProgressBar) {
         })
 
 }
+fun createNotificationChannel(context: Context, importance: Int, showBadge: Boolean, name: String, description: String) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channelId = "${context.packageName}-$name"
+        val channel = NotificationChannel(channelId, name, importance)
+        channel.description = description
+        channel.setShowBadge(showBadge)
+
+        val notificationManager = context.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
+}
+
--- app/src/main/java/com/ubaya/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ubaya/advweek4/view/StudentDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.ubaya.advweek4.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +13,13 @@ import com.ubaya.advweek4.R
 import com.ubaya.advweek4.util.loadImage
 import com.ubaya.advweek4.viewmodel.DetailViewModel
 import com.ubaya.advweek4.viewmodel.ListViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_student_detail.*
+import java.util.concurrent.TimeUnit
+
 
 class StudentDetailFragment : Fragment() {
     private lateinit var detailModel: DetailViewModel
@@ -44,6 +51,19 @@ class StudentDetailFragment : Fragment() {
             txtBod.setText(it.dob)
             txtPhone.setText(it.phone)
             imageView2.loadImage(it.photoUrl.toString(),progressBar2)
+
+            var student = it
+            btnNotif.setOnClickListener {
+                Observable.timer(5, TimeUnit.SECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        Log.d("Messages", "five seconds")
+                        MainActivity.showNotification(student.name.toString(),
+                            "A new notification created",
+                            R.drawable.ic_baseline_person_24)
+                    }
+            }
         })
     }
 }
